ffdad7f29f5cbea6bd541a55df967fcbc0d439b8
Markvitim <markvitim@gmail.com>
2022-02-17 17:03:30 +0300

    test with useJUnitPlatform()

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,15 +1,19 @@
 package ru.netology.service;
 
 import org.junit.Assert;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldRemain() {
+    public void shouldRemainUseJunitJupiter() {
         CashbackHackService service = new CashbackHackService();
-                Assert.assertEquals(0, service.remain(1000));
+                Assert.assertEquals(120, service.remain(2880));
+    }
+    @org.junit.Test
+    public void shouldRemainUseJunit4() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(120, service.remain(2880));
     }
 }
